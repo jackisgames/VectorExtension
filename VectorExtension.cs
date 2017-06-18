196c575291ea052e9f4de256959fea271acee456
Jacky Riawan <jackisgames@gmail.com>
2017-06-18 11:43:33 +0100

    Initial push -Swizzling for Vector3

---
+++ VectorExtension.cs
@@ -0,0 +1,220 @@
+using UnityEngine;
+public static class VectorExtension
+{
+	/// <summary>
+	/// return new vector with x,x,x
+	/// </summary>
+	public static Vector3 xxx(this Vector3 v)
+	{
+		return new Vector3(v.x, v.x, v.x);
+	}
+
+	/// <summary>
+	/// return new vector with x,x,y
+	/// </summary>
+	public static Vector3 xxy(this Vector3 v)
+	{
+		return new Vector3(v.x, v.x, v.y);
+	}
+
+	/// <summary>
+	/// return new vector with x,x,z
+	/// </summary>
+	public static Vector3 xxz(this Vector3 v)
+	{
+		return new Vector3(v.x, v.x, v.z);
+	}
+
+	/// <summary>
+	/// return new vector with x,y,x
+	/// </summary>
+	public static Vector3 xyx(this Vector3 v)
+	{
+		return new Vector3(v.x, v.y, v.x);
+	}
+
+	/// <summary>
+	/// return new vector with x,y,y
+	/// </summary>
+	public static Vector3 xyy(this Vector3 v)
+	{
+		return new Vector3(v.x, v.y, v.y);
+	}
+
+	/// <summary>
+	/// return new vector with x,y,z
+	/// </summary>
+	public static Vector3 xyz(this Vector3 v)
+	{
+		return new Vector3(v.x, v.y, v.z);
+	}
+
+	/// <summary>
+	/// return new vector with x,z,x
+	/// </summary>
+	public static Vector3 xzx(this Vector3 v)
+	{
+		return new Vector3(v.x, v.z, v.x);
+	}
+
+	/// <summary>
+	/// return new vector with x,z,y
+	/// </summary>
+	public static Vector3 xzy(this Vector3 v)
+	{
+		return new Vector3(v.x, v.z, v.y);
+	}
+
+	/// <summary>
+	/// return new vector with x,z,z
+	/// </summary>
+	public static Vector3 xzz(this Vector3 v)
+	{
+		return new Vector3(v.x, v.z, v.z);
+	}
+
+	/// <summary>
+	/// return new vector with y,x,x
+	/// </summary>
+	public static Vector3 yxx(this Vector3 v)
+	{
+		return new Vector3(v.y, v.x, v.x);
+	}
+
+	/// <summary>
+	/// return new vector with y,x,y
+	/// </summary>
+	public static Vector3 yxy(this Vector3 v)
+	{
+		return new Vector3(v.y, v.x, v.y);
+	}
+
+	/// <summary>
+	/// return new vector with y,x,z
+	/// </summary>
+	public static Vector3 yxz(this Vector3 v)
+	{
+		return new Vector3(v.y, v.x, v.z);
+	}
+
+	/// <summary>
+	/// return new vector with y,y,x
+	/// </summary>
+	public static Vector3 yyx(this Vector3 v)
+	{
+		return new Vector3(v.y, v.y, v.x);
+	}
+
+	/// <summary>
+	/// return new vector with y,y,y
+	/// </summary>
+	public static Vector3 yyy(this Vector3 v)
+	{
+		return new Vector3(v.y, v.y, v.y);
+	}
+
+	/// <summary>
+	/// return new vector with y,y,z
+	/// </summary>
+	public static Vector3 yyz(this Vector3 v)
+	{
+		return new Vector3(v.y, v.y, v.z);
+	}
+
+	/// <summary>
+	/// return new vector with y,z,x
+	/// </summary>
+	public static Vector3 yzx(this Vector3 v)
+	{
+		return new Vector3(v.y, v.z, v.x);
+	}
+
+	/// <summary>
+	/// return new vector with y,z,y
+	/// </summary>
+	public static Vector3 yzy(this Vector3 v)
+	{
+		return new Vector3(v.y, v.z, v.y);
+	}
+
+	/// <summary>
+	/// return new vector with y,z,z
+	/// </summary>
+	public static Vector3 yzz(this Vector3 v)
+	{
+		return new Vector3(v.y, v.z, v.z);
+	}
+
+	/// <summary>
+	/// return new vector with z,x,x
+	/// </summary>
+	public static Vector3 zxx(this Vector3 v)
+	{
+		return new Vector3(v.z, v.x, v.x);
+	}
+
+	/// <summary>
+	/// return new vector with z,x,y
+	/// </summary>
+	public static Vector3 zxy(this Vector3 v)
+	{
+		return new Vector3(v.z, v.x, v.y);
+	}
+
+	/// <summary>
+	/// return new vector with z,x,z
+	/// </summary>
+	public static Vector3 zxz(this Vector3 v)
+	{
+		return new Vector3(v.z, v.x, v.z);
+	}
+
+	/// <summary>
+	/// return new vector with z,y,x
+	/// </summary>
+	public static Vector3 zyx(this Vector3 v)
+	{
+		return new Vector3(v.z, v.y, v.x);
+	}
+
+	/// <summary>
+	/// return new vector with z,y,y
+	/// </summary>
+	public static Vector3 zyy(this Vector3 v)
+	{
+		return new Vector3(v.z, v.y, v.y);
+	}
+
+	/// <summary>
+	/// return new vector with z,y,z
+	/// </summary>
+	public static Vector3 zyz(this Vector3 v)
+	{
+		return new Vector3(v.z, v.y, v.z);
+	}
+
+	/// <summary>
+	/// return new vector with z,z,x
+	/// </summary>
+	public static Vector3 zzx(this Vector3 v)
+	{
+		return new Vector3(v.z, v.z, v.x);
+	}
+
+	/// <summary>
+	/// return new vector with z,z,y
+	/// </summary>
+	public static Vector3 zzy(this Vector3 v)
+	{
+		return new Vector3(v.z, v.z, v.y);
+	}
+
+	/// <summary>
+	/// return new vector with z,z,z
+	/// </summary>
+	public static Vector3 zzz(this Vector3 v)
+	{
+		return new Vector3(v.z, v.z, v.z);
+	}
+
+}
